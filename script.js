a24ce33e32c16c84f8983797b5837261a7f249e7
brettl1991 <brettl1991@gmail.com>
2022-03-22 16:51:11 +0000

    Handling an ESC keypress event

--- script.js
+++ script.js
@@ -20,3 +20,14 @@ for (let i = 0; i < btnsShowModal.length; i++)
 
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
+
+//how to find out when the esc key been pressed, js call this function when a keydown event happens and when you do so please pass in the event object as an argument
+document.addEventListener('keydown', function (e) {
+  //e stands for event
+  console.log(e); //creating this object: KeyboardEvent {isTrusted: true, key: 'Enter', code: 'Enter', location: 0, ctrlKey: false, …}
+  console.log(e.key); //getting back the key name that we pressed
+  ////we can check if an element has a certain class with .contains
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
