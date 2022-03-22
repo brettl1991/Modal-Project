b26c49aaab487ce475abac1d200e2abe071fb2dc
brettl1991 <brettl1991@gmail.com>
2022-03-22 13:33:01 +0000

    Working with classes

--- script.js
+++ script.js
@@ -3,8 +3,20 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnsShowModal = document.querySelectorAll('.show-modal');
-console.log(btnsShowModal);
 
-for (let i = 0; i < btnsShowModal.length; i++) {
-  console.log(btnsShowModal[i].textContent);
-}
+const showModal = function () {
+  console.log('Button clicked');
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+for (let i = 0; i < btnsShowModal.length; i++)
+  btnsShowModal[i].addEventListener('click', showModal);
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
